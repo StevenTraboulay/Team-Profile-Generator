65682b9fcf4380e354c35db905ff2eb2aa8f141a
StevenTraboulay <khaleeltraboulay@gmail.com>
2020-11-08 23:50:41 -0500

    fixes

--- src/page-template.js
+++ src/page-template.js
@@ -1,17 +1,18 @@
-const generateTeam = (team) => {
+//generate team
+const generateTeam = team => {
   //create the maneager html
-  const generateManager = (manager) => {
+  const generateManager = manager => {
     return `
     <div class ="card employee-card">
-    <dive class ="card-header>
+    <div class ="card-header">
         <h2 class ="card-title">${manager.getName()}</h2>
         <h3 class ="card-title"><i class ="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
     </div>
     <div class ="card-body">
         <ul class="list-group">
             <li class ="list-group-item">ID: ${manager.getId()}</li>
-            <li class ="list-group-item">Email: <a href=mailto:${manager.getEmail()}}">${manager.getEmail()}</a></li>
-            <li class ="list-group-item">Office number: ${manager.getOfficeNumber()}}</li>
+            <li class ="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+            <li class ="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
             </ul>
         </div>
     </div>
@@ -19,17 +20,17 @@ const generateTeam = (team) => {
   };
 
   //create html for engineers
-  const generateEngineer = (engineer) => {
+  const generateEngineer = engineer => {
     return `
     <div class ="card employee-card">
-    <dive class ="card-header>
+    <div class ="card-header">
         <h2 class ="card-title">${engineer.getName()}</h2>
         <h3 class ="card-title"><i class ="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
     </div>
     <div class ="card-body">
         <ul class="list-group">
             <li class ="list-group-item">ID: ${engineer.getId()}</li>
-            <li class ="list-group-item">Email: <a href=mailto:${engineer.getEmail()}}">${engineer.getEmail()}</a></li>
+            <li class ="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
             <li class ="list-group-item">GitHub: <a href ="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
             </ul>
         </div>
@@ -38,17 +39,17 @@ const generateTeam = (team) => {
   };
 
   //create html for interns
-  const generateIntern = (intern) => {
+  const generateIntern = intern => {
     return `
     <div class ="card employee-card">
-    <dive class ="card-header>
+    <div class ="card-header">
         <h2 class ="card-title">${intern.getName()}</h2>
-        <h3 class ="card-title"><i class ="fas fa-glasses mr-2"></i>${intern.getRole()}</h3>
+        <h3 class ="card-title"><i class ="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
     </div>
     <div class ="card-body">
         <ul class="list-group">
             <li class ="list-group-item">ID: ${intern.getId()}</li>
-            <li class ="list-group-item">Email: <a href=mailto:${intern.getEmail()}}">${intern.getEmail()}</a></li>
+            <li class ="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
             <li class ="list-group-item">School: ${intern.getSchool()}</li>
             </ul>
         </div>
@@ -77,11 +78,13 @@ const generateTeam = (team) => {
       .map((intern) => generateIntern(intern))
       .join("")
   );
-};
+
+  return html.join("");
+}
 
 //export function to gen page
 
-module.exports = (team) => {
+module.exports = team => {
   return `
 
 <!DOCTYPE html>
